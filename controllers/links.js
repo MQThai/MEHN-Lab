0574610c8e885bceeabbe91ee3c127e0bb4b1136
Minhquan Thai <minhquanthai@gmail.com>
2018-04-05 11:43:39 -0400

    Created show route

--- controllers/links.js
+++ controllers/links.js
@@ -2,4 +2,13 @@ const express = require('express')
 const router = express.Router()
 const Links = require('../models/links')
 
+router.get('/', (req, res) => {
+  Links.find({}).then(links => res.render('index', {links}))
+})
+
+router.get('/:id', (req, res) => {
+  Links.findOne({_id: req.params.id})
+    .then(link => res.render('show', {link}))
+})
+
 module.exports = router
