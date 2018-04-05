ae54c104ee86034f1160d45d9ec2e6c510edb71f
Minhquan Thai <minhquanthai@gmail.com>
2018-04-05 11:23:40 -0400

    Added controller

---
+++ controllers/links.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const router = express.Router()
+const Links = require('../models/links')
+
+module.exports = router
--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const hbs = require('hbs')
 
 const app = express()
 
+const linksController = require('./controllers/linksController')
+
 app.set('view engine', 'hbs')
+
+app.use('/', linksController)
 
 app.listen(3000, () => console.log('Running on port 3000!'))
